ebaa895308d2a4b62f809ac4368232e953a2ef2a
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-23 20:40:37 +0530

    header1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import './App.css';
-import Nav from "./components/Nav"
 import Home from "./components/Home"
-import Cart from "./components/Cart"
-import Details from "./components/Details";
+// import Cart from "./components/Cart"
+// import Details from "./components/Details";
 
 function App() {
   return (
