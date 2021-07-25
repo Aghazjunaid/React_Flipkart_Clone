674ae65003997782d7a073d9819b665d6ef1066c
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-25 16:19:52 +0530

    navbar

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -1,10 +1,46 @@
+import { Box, makeStyles, Typography } from '@material-ui/core';
 import React from 'react'
+import {navData} from '../../data';
+
+
+const useStyle = makeStyles(theme => ({
+    nav:{
+        display:"flex",
+        justifyContent: "space-between",
+        margin: "0px 130px"
+    },
+    container:{
+        textAlign: "center"
+    },
+    image: {
+        width: 64
+    },
+    text: {
+        fontSize: 14,
+        fontWeight: 600,
+        fontFamily: 'inherit'
+    }
+
+}));
 
 function Navbar() {
+    const classes = useStyle(); //To use css first call it
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <Box className={classes.nav}> 
+            {
+                navData.map(data=> (
+                    <Box className={classes.container}>
+                    <img src={data.url} className={classes.image} />
+                    <Typography className={classes.text}>{data.text}</Typography>
+                </Box>
+
+                ))
+            }
+
+            </Box>
+        </>
     )
 }
 
