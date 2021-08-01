b7b32fc7176fd435e6db5934b3d9d846163b2374
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-08-01 20:19:55 +0530

    responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,66 +1,112 @@
-import React from "react";
-import {AppBar, Toolbar, makeStyles, Box, Typography, withStyles} from '@material-ui/core';
-import SearchBar from "./SearchBar";
-import HeaderButton from "./HeaderButton";
+import { AppBar, Toolbar, makeStyles, Box, Typography, withStyles, IconButton, Drawer, List, ListItem} from '@material-ui/core';
 import { Link } from 'react-router-dom';
-
+import CustomButtons from './HeaderButton';
+import Search from './SearchBar';
+import { Menu } from '@material-ui/icons';
+import { useState } from 'react';
 
 const useStyle = makeStyles(theme => ({
-    header:{
-        backgroundColor: '#2874f0',
-        height:55
+    header: {
+        background: '#2874f0',
+        height: 55
     },
-    logo:{
-        width:75
+    component: {
+        marginLeft: '12%',
+        lineHeight: 0,
+        color: '#FFFFFF',
+        textDecoration: 'none'
     },
-    container:{
-        display: 'flex'
+    logo: {
+        width: 75
     },
-    subURL:{
-        width:10,
-        height: 10,
-        marginLeft:3
+    container: {
+        display: 'flex',
     },
-    component:{
-        marginLeft:"12%",
-        lineHeight:0
-    },
-    subHeading:{
+    subHeading: {
         fontSize: 10,
         fontStyle: 'italic'
+    },
+    subURL: {
+        width: 10,
+        height: 10,
+        marginLeft: 4
+    },
+    list: {
+        width: 250
+    },
+    menuButton: {
+        display: 'none',
+        [theme.breakpoints.down('sm')]: {
+            display: 'block'
+        }
+    },
+    customButtons: {
+        margin: '0 5% 0 auto', 
+        [theme.breakpoints.down('sm')]: {
+            display: 'none'
+        } 
     }
 }));
 
-const ToolBar = withStyles({ //to override pre written css use withstyles
+const ToolBar = withStyles({
     root: {
       minHeight: 55
     },
 })(Toolbar);
 
-// Use box as div and Typography Heading
-function Header() {
-    const classes = useStyle(); //To use css first call it
+const Header = () => {
+    const classes = useStyle();
     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
 
-  return (
-    <>
-      <AppBar position="fixed" className={classes.header}>
-        <ToolBar>
-            <Link to='/' className={classes.component}>
-                <img src={logoURL} className={classes.logo} />
-                <Box component="span" className={classes.container}>
-                    <Typography className = {classes.subHeading}>Explore <Box component="span" style={{color:'#FFE500'}}>Plus</Box></Typography>
-                    <img src={subURL} className={classes.subURL} />
-                </Box>
-            </Link>
-            <SearchBar/>
-            <HeaderButton/>
-        </ToolBar>
-      </AppBar>
-      <Toolbar />
-    </>
-  );
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const list = () => (
+        <Box className={classes.list} onClick={handleClose}>
+            <List>
+                <listItem button>
+                    <CustomButtons />
+                </listItem>
+            </List>
+        </Box>
+    );
+
+
+    return (
+        <AppBar position="fixed" className={classes.header}>
+            <ToolBar>
+                <IconButton
+                    color="inherit"
+                    className={classes.menuButton}
+                    onClick={handleOpen}
+                >
+                    <Menu />
+                </IconButton>
+
+                <Drawer open={open} onClose={handleClose}>
+                    {list()}
+                </Drawer>
+
+                <Link to='/' className={classes.component}>
+                    <img src={logoURL} className={classes.logo} />
+                    <Box component="span" className={classes.container}>
+                        <Typography className = {classes.subHeading}>Explore <Box component="span" style={{color:'#FFE500'}}>Plus</Box></Typography>
+                        <img src={subURL} className={classes.subURL} />
+                    </Box>
+                </Link>
+                <Search />
+                <span className={classes.customButtons}><CustomButtons /></span>
+            </ToolBar>
+        </AppBar>
+    )
 }
 
 export default Header;
